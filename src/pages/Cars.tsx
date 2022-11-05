95518e74dbe77536f929963e97bd6c75bf08864c
narges safari <narges.safari.8631@gmail.com>
2022-11-05 20:55:29 +0330

    feat: use static CarsData and map through data

--- src/pages/Cars.tsx
+++ src/pages/Cars.tsx
@@ -1,12 +1,15 @@
 import { Box, Grid } from "@mui/material";
 import Info from "../components/Info/Info";
+import CarsData from "../assets/data/data.json";
 import Statistic from "../components/Statistic/Statistic";
-import data from "../assets/data/data.json";
+import { useCars } from "./Car.biz";
 
 const Cars = () => {
+  const { colorRender } = useCars();
+
   return (
     <Box margin={3} borderRadius={4} padding={3}>
-      {data.map((item) => (
+      {CarsData.map((item) => (
         <Grid
           container
           spacing={3}
@@ -15,35 +18,24 @@ const Cars = () => {
           borderBottom={"2px solid #d9d8d8"}
           style={{ borderBottomLeftRadius: 40 }}
         >
-          <Grid item xs={4} display={"flex"} flexDirection={"column"}>
-            <Info title={item.title} description={item.description} />
+          <Grid item xs={5} display={"flex"} flexDirection={"column"}>
+            <Info
+              title={item.title}
+              description={item.description}
+              cars={item.cars}
+              uniqueValues={item.uniqueValues}
+            />
           </Grid>
-          <Grid item xs={8} display={"flex"} flexDirection={"column"}>
+          <Grid item xs={7} display={"flex"} flexDirection={"column"}>
             <Box width={"100%"} height={8} bgcolor={"#00c58d"} marginTop={3} />
-            <Statistic
-              name={"valid"}
-              color={"#00c58d"}
-              quantityOrMostCommen={7385}
-              percent={100}
-            />
-            <Statistic
-              name={"Mismatched"}
-              color={"#f97c00"}
-              quantityOrMostCommen={0}
-              percent={0}
-            />
-            <Statistic
-              name={"Missing"}
-              color={"#dc4e55"}
-              quantityOrMostCommen={0}
-              percent={0}
-            />
-            <Statistic name={"Unique"} quantityOrMostCommen={42} />
-            <Statistic
-              name={"Most Common"}
-              quantityOrMostCommen={"FORD"}
-              percent={9}
-            />
+            {item.statistics.map((sts) => (
+              <Statistic
+                name={sts.name}
+                color={colorRender(sts.name)}
+                detail={sts.detail}
+                percentage={sts.percentage}
+              />
+            ))}
           </Grid>
         </Grid>
       ))}
